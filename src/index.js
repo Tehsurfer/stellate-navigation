36f380a40b388d51312f8c242a9c7ba0c32641ca
Jesse Khorasanee <jessekhorasanee@gmail.com>
2019-08-20 11:35:38 +1200

    Upload stellate navigation package

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// index.js - Loads file structure and links the html elements to corresponding s3 urls
+const TreeView = require('js-treeview')
+const Plotsvy = require('plotsvy')
+var s3path = 'https://mapcore-bucket1.s3-us-west-2.amazonaws.com/ISAN/csv-data/stellate/'
+var fileStructureUrl = 'https://jsonstorage.net/api/items/stellate-file-structure'
+var treeData = undefined
+
+fetch(fileStructureUrl).then(response => {
+	response.json().then(json => {
+		treeData = json['data']
+    bootDemo()
+    bootPlotsvy()
+	})
+})
+
+function bootDemo() {
+    var tree = new TreeView([treeData], 'tree') // Create our directory tree navigation
+    tree.on('select', (ev) => {
+        url = s3path + ev.data.path
+        plotsvy.clearChart()
+        plotsvy.openCSV(url)
+    })
+}
+
+function bootPlotsvy(){
+    plotsvy = new Plotsvy.Plotsvy(document.getElementById('plotsvy-div'))
+}
+
+
+
